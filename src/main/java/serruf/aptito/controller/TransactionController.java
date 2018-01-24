2ef79235185bdf04b661ce1f8595bdfc5c6e7513
Pavel Shapenov <pavel.shapenov@pressindex.ru>
2018-01-24 18:57:07 +0500

    Релизовал все end-points

--- src/main/java/serruf/aptito/controller/TransactionController.java
+++ src/main/java/serruf/aptito/controller/TransactionController.java
@@ -1,24 +1,47 @@
 package serruf.aptito.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import serruf.aptito.model.Transaction;
+import serruf.aptito.repository.TransactionRepository;
 import serruf.aptito.service.TransactionService;
+
+import java.util.Date;
+
 
 @RestController
 public class TransactionController {
 
     private final TransactionService transactionService;
+    private final TransactionRepository transactionRepository;
 
     @Autowired
-    public TransactionController(TransactionService transactionService) {
+    public TransactionController(TransactionService transactionService, TransactionRepository transactionRepository) {
         this.transactionService = transactionService;
+        this.transactionRepository = transactionRepository;
     }
 
     @RequestMapping(path = "/order/{orderId}/pay", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void pay(@PathVariable("orderId") Long orderId, @RequestBody Transaction transaction) {
         transactionService.processTransaction(orderId, transaction);
+    }
+
+    @RequestMapping(path = "/transactions")
+    public Iterable<Transaction> transactions() {
+        return transactionRepository.findAll();
+    }
+
+    @RequestMapping(path = "/order/{orderId}/rest")
+    public int rest(@PathVariable("orderId") Long orderId) {
+        return transactionRepository.getRestSum(orderId);
+    }
+
+    @RequestMapping(path = "/statistics")
+    public int statistics(@RequestParam(name = "statDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date statDate,
+                          @RequestParam(name = "endDate") @DateTimeFormat(pattern="yyyy-MM-dd") Date endDate) {
+        return transactionRepository.getSumByPeriod(statDate, endDate);
     }
 }
--- src/main/java/serruf/aptito/repository/TransactionRepository.java
+++ src/main/java/serruf/aptito/repository/TransactionRepository.java
@@ -1,12 +1,23 @@
 package serruf.aptito.repository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Temporal;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import serruf.aptito.model.Transaction;
+
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 public interface TransactionRepository extends CrudRepository<Transaction, Long> {
 
     @Query("select coalesce(sum(t.amount), 0) from Transaction t where (t.order.id = :orderId)")
     public int getOrderSum(@Param("orderId") Long orderId);
+
+    @Query("select o.amount - coalesce(sum(t.amount), 0) from Order o join o.transactions t where (o.id = :orderId)")
+    public int getRestSum(@Param("orderId") Long orderId);
+
+    @Query("select coalesce(sum(t.amount), 0) from Transaction t where t.createdAt BETWEEN :startDate AND :endDate")
+    public int  getSumByPeriod(@Param("startDate") @Temporal(TemporalType.TIMESTAMP) Date startDate,
+                               @Param("endDate") @Temporal(TemporalType.TIMESTAMP) Date endDate);
 }
