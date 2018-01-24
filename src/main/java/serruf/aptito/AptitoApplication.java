dc04278faa4929d54cbb6f8d5f1de1d30114ea2e
Pavel Shapenov <pavel.shapenov@pressindex.ru>
2018-01-24 18:59:54 +0500

    Два пути для поиска properties

--- src/main/java/serruf/aptito/AptitoApplication.java
+++ src/main/java/serruf/aptito/AptitoApplication.java
@@ -2,8 +2,14 @@ package serruf.aptito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
+		@PropertySource(value = "file:/etc/aptito/application.properties", ignoreResourceNotFound = true)
+})
 public class AptitoApplication {
 
 	public static void main(String[] args) {
